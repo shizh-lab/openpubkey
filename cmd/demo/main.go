ed15173e7b3794a7a97f375c00163a599133adda
Shiwei Zhang <shizh@microsoft.com>
2023-10-09 15:48:06 +0800

    demo

---
+++ cmd/demo/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/openpubkey/openpubkey/parties"
+	"github.com/openpubkey/openpubkey/pktoken"
+)
+
+func main() {
+	signer, err := pktoken.NewSigner("./pktoken.cfg", "ES256", true, map[string]any{"extra": "yes"})
+	if err != nil {
+		panic(err)
+	}
+	op, err := parties.NewGithubOpFromEnvironment()
+	if err != nil {
+		panic(err)
+	}
+	client := &parties.OpkClient{
+		Op:     op,
+		Signer: signer,
+	}
+	pktBytes, err := client.OidcAuth()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("---")
+	os.Stdout.Write(pktBytes)
+	fmt.Println("")
+	fmt.Println("---")
+}
